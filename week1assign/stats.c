3f2921bf316a75fca70e2ebae852bc08d918a672
attonasi <attonasi@hotmail.com>
2020-03-01 17:33:27 -0800

    commit #3

--- week1assign/stats.c
+++ week1assign/stats.c
@@ -19,15 +19,16 @@
  * @date March 1st, 2020
  */
 
-
-
 #include <stdio.h>
 #include "stats.h"
 
 /* Size of the Data Set */
+//#define SIZE 4
 #define SIZE (40)
 
 void main() {
+
+//    unsigned char test[SIZE] = {1,6,3,2};
 
   unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
                               114, 88,   45,  76, 123,  87,  25,  23,
@@ -37,14 +38,117 @@ void main() {
 
   /* Other Variable Declarations Go Here */
 
-  Statistical_Values * stats = create_stats(NULL, NULL, NULL, NULL);
+  Statistical_Values * stats = create_stats(0, 0, 0, 0);
 
   /* Statistics and Printing Functions Go Here */
 
+  print_array(test, SIZE);
 
+  stats->median = find_median(test, SIZE);
+  stats->mean = find_mean(test, SIZE);
+  stats->max = find_maximum(test, SIZE);
+  stats->min = find_minimum(test, SIZE);
 
+  print_statistics(stats);
+
+  /* Should be Sorted */
+  print_array(test, SIZE);
 
   free(stats);
 }
 
 /* Add other Implementation File Code Here */
+
+void print_statistics(Statistical_Values * stats) {
+
+    printf(" \n Maximum value:\t %u", stats->max);
+    printf(" \n Minimum value:\t %u", stats->min);
+    printf(" \n Mean value:\t %u", stats->mean);
+    printf(" \n Median value:\t %u", stats->median);
+}
+
+void print_array(unsigned char * nums, int len) {
+
+    printf("\n********* Array Elements **********\n\n[ ");
+    for ( int i = 0; i < len; i++) {
+        if (i==len-1) {
+            printf("%u ]", nums[i]);
+            break;
+        }
+        if ( i%10==0 && i>0) {
+            printf("\n");
+        }
+        printf("%u, ", nums[i]);
+    }
+    printf("\n\n ******************************* \n");
+}
+
+unsigned char find_median(unsigned char * nums, int len) {
+    sort_array(nums, len);
+
+    return nums[len/2];
+}
+
+unsigned char find_mean(unsigned char * nums, int len) {
+
+    int sum = 0;
+    for ( int i = 0; i < len; i++) {
+        sum = sum + nums[i];
+    }
+    unsigned char ret = sum/len;
+    return ret;
+}
+
+unsigned char find_maximum(unsigned char * nums, int len) {
+    unsigned char max = 0;
+
+    for (int i = 0; i < len; i++) {
+        if (nums[i] > max) { max = nums[i]; }
+    }
+    return max;
+}
+
+unsigned char find_minimum(unsigned char * nums, int len) {
+    unsigned char min = 255;
+
+    for (int i = 0; i < len; i++) {
+        if (nums[i] < min) { min = nums[i]; }
+    }
+    return min;
+}
+
+void sort_array(unsigned char * nums, int len) {
+    qsort_array(nums, 0, len-1);
+}
+
+/* qsort implementation */
+
+void qsort_array(unsigned char * nums, int low, int high) {
+
+    if (low < high) {
+        int pivot_index = qsort_partition(nums, low, high);
+
+        qsort_array(nums, low, pivot_index-1);
+        qsort_array(nums, pivot_index+1, high);
+    }
+}
+
+int qsort_partition(unsigned char * nums, int low, int high) {
+
+    int i = low;
+    for (int j = low; j <= high; j++) {
+        if (nums[j] < nums[high]) {
+            swap(&nums[i], &nums[j]);
+            i++;
+        }
+    }
+    swap(&nums[i], &nums[high]);
+    return i;
+}
+
+void swap(unsigned char* a, unsigned char* b){
+    if(*a==*b){ return; }
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
--- week1assign/stats.h
+++ week1assign/stats.h
@@ -9,19 +9,21 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stat.h
+ * @brief Header file for the stats.c file.
  *
- * <Add Extended Description Here>
+ * Contains structs and function headers for the stats.h file.
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Collin Smith
+ * @date March 1st, 2020
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+/* Add Your Declarations and Function Comments here */
+
+#include <stdlib.h>
 
 /**
  * @brief <Add Brief Description of Function Here>
@@ -37,8 +39,8 @@
  */
 
 /**
- * @brief Statistical_Values struct holds common statistical values derived
- * from an array.
+ * @brief Statistical_Values struct holds common statistical values
+ * derived from an array.
  *
  * The primary purpose is to reduce the number of parameters
  * in key function definitions. This Struct definition includes
@@ -58,21 +60,21 @@ typedef struct {
     unsigned char max;
     unsigned char min;
     unsigned char mean;
-    unsigned char mode;
+    unsigned char median;
 } Statistical_Values;
 
 Statistical_Values* create_stats(unsigned char max, unsigned char min,
-        unsigned char mean, unsigned char mode);
+        unsigned char mean, unsigned char median);
 
 Statistical_Values* create_stats(unsigned char max, unsigned char min,
-                                 unsigned char mean, unsigned char mode){
+                                 unsigned char mean, unsigned char median){
 
-    Statistical_Values* v = (Statistical_Values) malloc(sizeof(Statistical_Values));
+    Statistical_Values* v = malloc(sizeof(Statistical_Values));
 
     v->max = max;
     v->min = min;
     v->mean = mean;
-    v->mode = mode;
+    v->median = median;
 
     return v;
 }
@@ -83,11 +85,11 @@ Statistical_Values* create_stats(unsigned char max, unsigned char min,
  * This function will print the mean, median, max and min values of
  * a given array.
  *
- * @param stats void* to a struct holding the statistical derivatives
+ * @param stats Statistical_Values* to a struct holding the statistical derivatives
  * of a given array.
  */
 
-void print_statistics(void* stats);
+void print_statistics(Statistical_Values* stats);
 
 /**
  * @brief Prints the values of an array.
@@ -102,7 +104,8 @@ void print_array(unsigned char * nums, int len);
  * @brief Calculates the median value of the array.
  *
  * This is going to return the len/2 value of the array after
- * it has been sorted.
+ * it has been sorted. I am still trying to think of something
+ * different but all solutions are at least n lon n so far.
  *
  * @param unsigned char * nums pointer to the array.
  * @param int len number of elements in the array.
@@ -161,14 +164,38 @@ unsigned char find_minimum(unsigned char * nums, int len);
 void sort_array(unsigned char * nums, int len);
 
 /**
- * @brief  Sorts the array descending using qsort algorithm.
+ * @brief  Sorts the array descending in place using qsort algorithm.
+ *
+ * @param unsigned char * nums pointer to the array.
+ * @param int high higher bound index to .
+ * @param int low lower bound index to be sorted.
+ */
+void qsort_array(unsigned char * nums, int low, int high);
+
+/**
+ * @brief  QSort Partition helper puts the pivot number in the right place.
+ *
+ * This is the method that takes the pivot value and puts elements
+ * less than the pivot on the left and elements greater than the
+ * pivot on the right of the pivot in the array before placing the
+ * pivot.
  *
  * @param unsigned char * nums pointer to the array.
  * @param int high higher bound index to .
  * @param int low lower bound index to be sorted.
  *
- * @return unsigned char max value of numbers in the array
+ * @return int New partition index
+ *
  */
-void qsort_array(unsigned char * nums, int high, int low);
+int qsort_partition(unsigned char * nums, int low, int high);
+
+/**
+ * @brief  Helper to swap 2 numbers by reference.
+ *
+ * @param int a number to be swapped
+ * @param int b number to be swapped
+ *
+ */
+void swap(unsigned char*a, unsigned char*b);
 
 #endif /* __STATS_H__ */
